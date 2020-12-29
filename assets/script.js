23188234af27d20d4722217dae3bc218a43907d5
Natalia <nasozontova@gmail.com>
2020-12-28 23:58:10 -0500

    local storage methods

--- assets/script.js
+++ assets/script.js
@@ -3,20 +3,37 @@ var search = document.getElementById("search");
 
 
 var city;
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
 
 var today =  moment().format("("+'L'+")");
 
 // var day = moment().add(1, 'days').format("("+'L'+")");  
 // console.log(day); 
 
+cityHistory(searchHistory);
+
 $("#submit").on("click", function (event) {
- event.preventDefault();
+event.preventDefault();
 city = search.value.trim();
 console.log(city);
 date.text(city + " " + today);
+getForcast(city);
+checkHistory(city);
 
+});
+
+$("body").on("click",".list-group-item", function (event) {
+    event.preventDefault();
+    city = $(this).text();
+    console.log(city);
+    date.text(city + " " + today);
+    getForcast(city);
+    checkHistory(city); 
+    });
+
+function getForcast(city){
 var key= "ee832b09a8728a9c7e626c00b6d86173";
-
 var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + key;
  
  $.ajax({
@@ -27,6 +44,8 @@ var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&
     .then(function(response) {
       console.log(queryURL);
       console.log(response);
+
+      //for loop with i+8
    var temperature = JSON.stringify(response.list[3].main.temp);
    $("#temp1").text("Temp: " + temperature + " F");
    var humidity = JSON.stringify(response.list[3].main.humidity);
@@ -60,9 +79,43 @@ var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&
     
 });
 
-});
+}
+
+// storage
+function cityHistory(searchHistory){
+    $("#history").empty();
+    console.log(searchHistory);
+    for(var i=0; i<searchHistory.length; i++){
+        var item = $("<li>");
+        item.addClass("list-group-item");
+        item.text(searchHistory[i]);
+        $("#history").append(item);
+    }
+
+}
+
+function checkHistory(city){
+    if(city){
+    for(var i=0; i<searchHistory.length; i++){
+        if(searchHistory[i].toLowerCase() === city.toLowerCase()){
+            searchHistory.splice(i, 1);
+        }
+     }
+    
+    searchHistory.push(city);
+    localStorage.setItem("searchHistory",JSON.stringify(searchHistory));
+    cityHistory(searchHistory);
+    }
+}
 
 
+
+
+
+// weather.main: "Clouds"
+// main: "Snow"
+// main: "Clear"
+// main: "Rain"
 
 
 
